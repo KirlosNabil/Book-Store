411b2a5219d2efdfe64c6d4aea60c9d9bc8f5e8a
KirlosNabil <kirlosnabil956@gmail.com>
2024-02-02 06:08:29 +0200

    Added some css

--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -10,6 +10,14 @@ namespace BulkyWeb.Data
             
         }
         public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, DisplayOrder = 1, Name = "ShortStories" },
+                new Category { Id = 2, DisplayOrder = 2, Name = "Romantic" },
+                new Category { Id = 3, DisplayOrder = 3, Name = "Action" }
+                );
+        }
 
     }
 }
