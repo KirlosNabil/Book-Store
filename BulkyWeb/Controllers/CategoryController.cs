1d4fa7af1b1107ba44a0b5697602dbb83bcd7e6f
KirlosNabil <kirlosnabil956@gmail.com>
2024-02-03 07:08:36 +0200

    Added edit and delete for category

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -17,5 +17,72 @@ namespace BulkyWeb.Controllers
             List<Category> categoryList = _db.Categories.ToList();
             return View(categoryList);
         }
-    }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category newCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(newCategory);
+                _db.SaveChanges();
+				TempData["DoneSuccessfully"] = "Category created successfully!";
+				return RedirectToAction("Index");
+            }
+            return View();
+        }
+		public IActionResult Edit(int Id)
+		{
+            if(Id == 0)
+            {
+                return NotFound();
+            }
+            Category categoryToBeEdited = _db.Categories.Find(Id);
+            if(categoryToBeEdited == null)
+            {
+                return NotFound();
+            }
+			return View(categoryToBeEdited);
+		}
+		[HttpPost]
+		public IActionResult Edit(Category categoryToBeEdited)
+		{
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Update(categoryToBeEdited);
+				_db.SaveChanges();
+				TempData["DoneSuccessfully"] = "Category updated successfully!";
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+		public IActionResult Delete(int Id)
+		{
+			if (Id == 0)
+			{
+				return NotFound();
+			}
+			Category categoryToBeEdited = _db.Categories.Find(Id);
+			if (categoryToBeEdited == null)
+			{
+				return NotFound();
+			}
+			return View(categoryToBeEdited);
+		}
+		[HttpPost,ActionName("Delete")]
+		public IActionResult DeleteCategory(int? Id)
+		{
+			Category? categoryToBeDeleted = _db.Categories.Find(Id);
+			if (categoryToBeDeleted == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(categoryToBeDeleted);
+			_db.SaveChanges();
+            TempData["DoneSuccessfully"] = "Category deleted successfully!";
+			return RedirectToAction("Index");
+		}
+	}
 }
--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Models
 {
@@ -7,7 +9,11 @@ namespace BulkyWeb.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [DisplayName("Category Name")]
+        [MaxLength(25)]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,50, ErrorMessage = "Invalid display order, range is 1-100")]
         public int DisplayOrder { get; set; }
 
     }
