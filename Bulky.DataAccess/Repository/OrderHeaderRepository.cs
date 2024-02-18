3663effeb8c196fa3d0e0976697e4ca51d62ff22
KirlosNabil <kirlosnabil956@gmail.com>
2024-02-18 02:48:24 +0200

    added stripe and payment validation

--- Bulky.DataAccess/Repository/OrderHeaderRepository.cs
+++ Bulky.DataAccess/Repository/OrderHeaderRepository.cs
@@ -21,5 +21,30 @@ namespace Bulky.DataAccess.Repository
 		{
 			_db.Update(orderHeader);
 		}
+		public void UpdateStatus(int id, string orderStatus, string? paymentStatus = null)
+		{
+			var getOrder = _db.OrderHeaders.FirstOrDefault(x => x.Id == id);
+			if(getOrder != null)
+			{
+				getOrder.OrderStatus = orderStatus;
+				if(!string.IsNullOrEmpty(paymentStatus))
+				{
+					getOrder.PaymentStatus = paymentStatus;
+				}
+			}
+		}
+		public void UpdateStripePaymentId(int id, string sessionId, string paymentIntentId)
+		{
+			var getOrder = _db.OrderHeaders.FirstOrDefault(x => x.Id == id);
+			if(!string.IsNullOrEmpty(sessionId))
+			{
+				getOrder.SessionId = sessionId;
+			}
+			if(!string.IsNullOrEmpty(paymentIntentId))
+			{
+				getOrder.PaymentIntentId = paymentIntentId;
+				getOrder.PaymentDate = DateTime.Now;
+			}
+		}
 	}
 }
