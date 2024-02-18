900037db8769c6347cce9e1fc350400412fd120b
KirlosNabil <kirlosnabil956@gmail.com>
2024-02-18 04:51:39 +0200

    added order model, view and controller

---
+++ BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,34 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Bulky.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class OrderController : Controller
+	{
+		public readonly IUnitOfWork _UnitOfWork;
+		public OrderController(IUnitOfWork unitOfWork)
+		{
+			_UnitOfWork = unitOfWork;
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+		#region API CALLS
+
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			List<OrderHeader> orderList = _UnitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+			return Json(new { data = orderList });
+		}
+
+		#endregion
+	}
+}
